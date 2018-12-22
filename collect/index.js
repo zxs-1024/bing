9582d630b866e4dd067b340c8b1f5e049dde31d0
hao.zhang <735296233@qq.com>
2018-12-22 11:55:17 +0800

    :art: art code

--- collect/index.js
+++ collect/index.js
@@ -4,86 +4,103 @@ const { promisify } = require('util')
 
 const writeFile = promisify(fs.writeFile)
 const mkdir = promisify(fs.mkdir)
-
-const { fillZero } = require('../utils')
+const { fillZero, getMonthDays } = require('../utils')
 const times = require('../utils/times')
 
 const baseUrl = 'http://bingwallpaper.anerg.com/cn/'
 const collectPath = './collect/data'
 
-// 跳转页面，爬取数据
-const puppeteerFn = async (page, time) => {
-  await page.goto(`${baseUrl}${time}`)
-
-  const evaluate = await page.evaluate(() => {
-    // 获取图片 DOM
-    const images = document.querySelectorAll('#photos .panel a img')
-    const collect = [...images]
-
-    // http://bingwallpaper.anerg.com/cn/201812
-    // => 201812 => 12
-    const pathname = window.location.pathname.split('/')[2]
-    const month = pathname.slice(4, 6)
-    const now = new Date()
-    const nowMonth = now.getMonth() + 1
-    const nowDay = now.getDate()
-    const nowTime = `${now.getFullYear()}${fillZero(nowMonth)}`
-
-    function fillZero(number) {
-      return number < 10 ? `0${number}` : number
-    }
-
-    // 获取当月天数
-    const date = new Date(`${pathname}`)
-    date.setMonth(month)
-    date.setDate(0)
-    let day = date.getDate()
-
-    if (nowTime === pathname) day = nowDay
-
-    return collect.map(({ src: url, alt: copyright }) => {
-      const fillDay = fillZero(day--)
-      const date = new Date(
-        `${pathname.slice(0, 4)}-${pathname.slice(4, 6)}-${fillDay}`
-      ).getTime()
-      const name = url.replace(
-        /(http:\/\/cdn.nanxiongnandi.com\/bing\/|_1366x768.jpg)/g,
-        ''
-      )
-      const dateString = `${pathname}${fillDay}`
-      return { dateString, date, url, name, copyright }
-    })
-  })
-
-  if (!fs.existsSync(collectPath)) {
-    await mkdir(collectPath).then(() =>
-      console.log(`📂  创建 ${collectPath} 文件夹成功！`)
-    )
-  }
-
-  // 格式化写入 JSON 文件
-  const data = JSON.stringify(evaluate, null, 2)
-  await writeFile(`${collectPath}/${time}.json`, data).then(() => {
-    console.log(`📄  写入 ${time}.json 文件成功！`)
-  })
-}
-
 ;(async () => {
+  // 启动浏览器
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     timeout: 0,
     ignoreHTTPSErrors: true
   })
 
+  // 打开页面
   const page = await browser.newPage()
 
   // 遍历时间数组，爬取数据
   for (let i = 0; i < times.length; i++) {
     if (!fs.existsSync(`${collectPath}/${times[i]}.json`)) {
-      await puppeteerFn(page, times[i])
+      // 收集图片信息
+      const evaluate = await puppeteerFn(page, times[i])
+      // 处理数据
+      const collect = handleTransCollect(evaluate, times[i])
+      // 写入 JSON 文件
+      await handleWriteFile(collect, times[i])
     }
   }
 
   console.log('🎉  你的数据已爬取完毕 => 冲鸭！！！')
   await browser.close()
 })()
+
+// 写入 JSON 文件
+async function handleWriteFile(evaluate, time) {
+  if (!fs.existsSync(collectPath)) {
+    await mkdir(collectPath).then(() =>
+      console.log(`📂  创建 ${collectPath} 文件夹成功！`)
+    )
+  }
+
+  // 格式化
+  const data = JSON.stringify(evaluate, null, 2)
+  await writeFile(`${collectPath}/${time}.json`, data).then(() => {
+    console.log(`📄  写入 ${time}.json 文件成功！`)
+  })
+}
+
+// 收集图片信息
+async function puppeteerFn(page, time) {
+  // 输入地址
+  await page.goto(`${baseUrl}${time}`)
+
+  // 等待 img 渲染完毕
+  await page.waitForSelector('#photos .panel a img')
+
+  // 页面加载后执行回调，收集图片信息 => 在浏览器环境
+  return await page.evaluate(month => {
+    const images = document.querySelectorAll('#photos .panel a img')
+    return [...images].map(({ src: url, alt: copyright }) => {
+      return {
+        url,
+        copyright,
+        month
+      }
+    })
+  }, time)
+}
+
+// 处理数据
+function handleTransCollect(collect, month) {
+  const now = new Date()
+  const nowMonth = now.getMonth() + 1
+  const nowDay = now.getDate()
+  const nowTime = `${now.getFullYear()}${fillZero(nowMonth)}`
+
+  // 获取当月天数
+  let day = getMonthDays(month)
+
+  if (nowTime === month) day = nowDay
+
+  return collect.map(({ url, copyright }) => {
+    const fillDay = fillZero(day--)
+    const date = new Date(
+      `${month.slice(0, 4)}-${month.slice(4, 6)}-${fillDay}`
+    ).getTime()
+    const name = url.replace(
+      /(http:\/\/cdn.nanxiongnandi.com\/bing\/|_1366x768.jpg)/g,
+      ''
+    )
+    const dateString = `${month}${fillDay}`
+    return {
+      dateString,
+      date,
+      url,
+      name,
+      copyright
+    }
+  })
+}
--- utils/index.js
+++ utils/index.js
@@ -6,7 +6,16 @@ const sleep = time => {
   return new Promise(resolve => setTimeout(() => resolve(), time))
 }
 
+const getMonthDays = time => {
+  const date = new Date(`${time}`)
+  const month = String(time).slice(4, 6)
+  date.setMonth(month)
+  date.setDate(0)
+  return date.getDate()
+}
+
 module.exports = {
   fillZero,
-  sleep
+  sleep,
+  getMonthDays
 }
